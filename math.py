ea038a19724771682d1df9d3610d9dd0f25a3e0f
bahni <bahninfe98@gmail.com>
2021-08-30 16:50:17 +0530

    Implemented multiply fn from local repo

--- math.py
+++ math.py
@@ -6,7 +6,7 @@ def subtract(x,y):
     return x-y           #from remote_repo
   #Multiply implementation                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 def multiply(x,y):
-    pass
+    return x*y        #From Local repo
   #Divide implementation                                                       
 def divide(x,y):
      pass
